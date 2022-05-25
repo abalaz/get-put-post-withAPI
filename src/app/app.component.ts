c1265158ea1c7447ba1ffb041295082efb6381b0
Huy Nguyen Gia <intern.ngiahuy1@tma.com.vn>
2022-05-25 17:20:41 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Blog, PostServiceService } from './post-service.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'http';
-}
+export class AppComponent implements OnInit {
+  public listBlog: Blog[] = [];
+  public content = '';
+  public id ='';
+  public title='';
+  public blog:Blog[]=[];
+url = `https://628c5bd03df57e983ecd72ea.mockapi.io/api/v1/blog/`;
+  isEdited = false;
+  constructor(private postServiceService: PostServiceService ) {
+  }
+
+  ngOnInit(): void {
+      this.postServiceService.getListBlog().subscribe((res) =>{
+        this.listBlog = res;
+        console.log('this.listBlog',this.listBlog);
+
+      })
+  }
+
+  deleteMe(id: string): void {
+    this.postServiceService.deleteBlogItem(id).subscribe(res =>{
+      console.log(res);
+      window.location.reload();
+    })
+  }
+  add(): void{
+    this.postServiceService.postBlogItem(blog).subscribe(res =>{
+      console.log(res);
+    })
+    }
+  }
+
---
+++ src/app/post-service.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, Observable, tap } from 'rxjs';
+
+
+export interface Blog {
+  content: string;
+  createdAt: string;
+  description: string;
+  id: string;
+  img: string;
+  thumbnail: string;
+  title: string;
+};
+
+const httpOptions = {
+  Headers: new HttpHeaders({'Content-Type':'application/json'})
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostServiceService {
+  [x: string]: any;
+  url = `https://628c5bd03df57e983ecd72ea.mockapi.io/api/v1/blog/`;
+  constructor(private http: HttpClient) {
+
+   }
+
+  getListBlog(): Observable<any | Blog[]> {
+   return this.http.get(this.url).pipe(first());
+  }
+
+  deleteBlogItem(id:string): Observable<any | Blog> {
+    console.log('id', id)
+    return this.http.delete(`${this.url + id}`).pipe(first());
+  }
+
+  postBlogItem(blog: Blog ):Observable<any | Blog[]> {
+      return this.http.post(this.url, blog).pipe(first());
+  }
+}
